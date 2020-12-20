51eccf18cfdae5ee8b1db0d24d7dbb5d6fc37d95
Milena Grncarova <milenagrncarov@outlook.com>
2020-12-20 16:55:06 +0100

    Made all the routes to all pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#show", page: "home"
   get "/pages/:page" => "pages#show"
+  get "about", to: "pages#about"
+  get "resume", to: "pages#resume"
+  get "podcast", to: "pages#podcast"
+  get "youtube", to: "pages#youtube"
 end
